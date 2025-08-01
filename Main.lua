a77f4fd71de1d5fef58eb2379314e30c23ffa349
WooblyWibbly <verify.univer@gmail.com>
2025-08-01 20:15:45 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 0 then
 
 else
-  loadstring(game:HttpGet("")()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/WooblyWibbly/Main-Scripts/refs/heads/main/Universal.lua")()
 end
